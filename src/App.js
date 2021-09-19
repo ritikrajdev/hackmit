68ea10fa31f72d40216482245050cfe9cc40c4f2
ritikrajdev <ritikrajdev761@gmail.com>
2021-09-19 16:43:34 +0530

    Basic done

---
+++ src/App.js
@@ -0,0 +1,142 @@
+// Import dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import Webcam from "react-webcam";
+import "./App.css";
+import { nextFrame } from "@tensorflow/tfjs";
+import {MuiChat, ChatController} from "chat-ui-react";
+// 2. TODO - Import drawing utility here
+// e.g. import { drawRect } from "./utilities";
+import {drawRect} from "./utilities"; 
+
+function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [chatCtl] = React.useState(new ChatController());
+
+  let mes = [{message: '',reply: ''}];
+  let last_mes = JSON.parse(JSON.stringify(mes));
+
+  // Main function
+  const runCoco = async () => {
+    // 3. TODO - Load network 
+    // e.g. const net = await cocossd.load();
+    // https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json
+    const net = await tf.loadGraphModel('https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json')
+    
+    //  Loop and detect hands
+    setInterval(() => {
+      detect(net);
+    }, 1000);
+  };
+
+  const detect = async (net) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // 4. TODO - Make Detections
+      const img = tf.browser.fromPixels(video)
+      const resized = tf.image.resizeBilinear(img, [640,480])
+      const casted = resized.cast('int32')
+      const expanded = casted.expandDims(0)
+      const obj = await net.executeAsync(expanded)
+
+      const boxes = await obj[1].array()
+      const classes = await obj[2].array()
+      const scores = await obj[4].array()
+      
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+
+      // 5. TODO - Update drawing utility
+      // drawSomething(obj, ctx)
+      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.75, videoWidth, videoHeight, ctx, mes)});
+
+      if (JSON.stringify(last_mes) !== JSON.stringify(mes)) {
+        chatCtl.addMessage({
+            type: 'text',
+            content: mes[0].message,
+            self: true,
+        });
+
+        chatCtl.addMessage({
+            type: 'text',
+            content: mes[0].reply,
+            self: false,
+        });
+
+        last_mes = JSON.parse(JSON.stringify(mes));
+      }
+
+      
+      tf.dispose(img)
+      tf.dispose(resized)
+      tf.dispose(casted)
+      tf.dispose(expanded)
+      tf.dispose(obj)
+      console.log(mes)
+
+    }
+  };
+
+  useEffect(()=>{runCoco()},[]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            zindex: 9,
+            width: window.innerWidth / 2,
+            height: window.innerHeight,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            zindex: 8,
+            width: (window.innerWidth / 2) - 20,
+            height: window.innerHeight - 2,
+          }}
+        />
+      </header>
+      <div 
+        style={{
+          display: 'inline-block',
+          backgroundColor: 'red',
+            width: (window.innerWidth / 2) - 20,
+            height: window.innerHeight - 2,
+            cssFloat: 'right',
+        }}>
+          <MuiChat chatController={chatCtl} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilities.js
@@ -0,0 +1,37 @@
+// Define our labelmap
+const labelMap = {
+    1:{name:'Hello', color:'red', reply:'Hi'},
+    2:{name:'Thank You', color:'yellow', reply:'Your Welcome'},
+    3:{name:'I Love You', color:'lime', reply:'Love Yaaaaa !!!'},
+    4:{name:'Location', color:'blue', reply:'Locationnnn'},
+    5:{name:'Doctor', color:'purple', reply:'Calling'},
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx, mes)=>{
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'white'
+            ctx.font = '30px Arial'
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/1.5);
+            ctx.stroke()
+
+            if (mes.at(-1)['reply'] != labelMap[text]['reply']) {
+                mes.push({message: labelMap[text]['name'],reply:labelMap[text]['reply']})
+                if (mes.length > 1)
+                    mes.splice(0,1)
+            }
+        }
+    }
+}
