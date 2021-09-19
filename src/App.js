bb71fc5ce4759e935cee14e214db8037c20ecc1d
Basavraj Chinagundi <75333151+basavraj-chinagundi@users.noreply.github.com>
2021-09-19 19:09:50 +0530

    Update pushed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import "./App.css";
 import { nextFrame } from "@tensorflow/tfjs";
-import {MuiChat, ChatController} from "chat-ui-react";
+import { MuiChat, ChatController } from "chat-ui-react";
 // 2. TODO - Import drawing utility here
 // e.g. import { drawRect } from "./utilities";
-import {drawRect} from "./utilities"; 
+import { drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
   const [chatCtl] = React.useState(new ChatController());
 
-  let mes = [{message: '',reply: ''}];
+  let mes = [{ message: '', reply: '' }];
   let last_mes = JSON.parse(JSON.stringify(mes));
 
   // Main function
@@ -23,7 +23,7 @@ function App() {
     // e.g. const net = await cocossd.load();
     // https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json
     const net = await tf.loadGraphModel('https://tensorflowjsrealtimemodel.s3.au-syd.cloud-object-storage.appdomain.cloud/model.json')
-    
+
     //  Loop and detect hands
     setInterval(() => {
       detect(net);
@@ -52,7 +52,7 @@ function App() {
 
       // 4. TODO - Make Detections
       const img = tf.browser.fromPixels(video)
-      const resized = tf.image.resizeBilinear(img, [640,480])
+      const resized = tf.image.resizeBilinear(img, [640, 480])
       const casted = resized.cast('int32')
       const expanded = casted.expandDims(0)
       const obj = await net.executeAsync(expanded)
@@ -60,31 +60,31 @@ function App() {
       const boxes = await obj[1].array()
       const classes = await obj[2].array()
       const scores = await obj[4].array()
-      
+
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)
-      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.75, videoWidth, videoHeight, ctx, mes)});
+      requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.75, videoWidth, videoHeight, ctx, mes) });
 
       if (JSON.stringify(last_mes) !== JSON.stringify(mes)) {
         chatCtl.addMessage({
-            type: 'text',
-            content: mes[0].message,
-            self: true,
+          type: 'text',
+          content: mes[0].message,
+          self: true,
         });
 
         chatCtl.addMessage({
-            type: 'text',
-            content: mes[0].reply,
-            self: false,
+          type: 'text',
+          content: mes[0].reply,
+          self: false,
         });
 
         last_mes = JSON.parse(JSON.stringify(mes));
       }
 
-      
+
       tf.dispose(img)
       tf.dispose(resized)
       tf.dispose(casted)
@@ -95,14 +95,30 @@ function App() {
     }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(() => { runCoco() }, []);
+
+  React.useMemo(async () => {
+    // Chat content is displayed using ChatController
+    await chatCtl.addMessage({
+      type: 'text',
+      content: `Welcome to hushVoice.`,
+      self: false,
+    });
+    await chatCtl.addMessage({
+      type: 'text',
+      content: `Use sign language to interact with our AI chat assistant.`,
+      self: false,
+    });
+  }, [chatCtl]);
+
+
 
   return (
     <div className="App">
       <header className="App-header">
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
           style={{
             position: "absolute",
             left: 0,
@@ -125,15 +141,15 @@ function App() {
           }}
         />
       </header>
-      <div 
+      <div
         style={{
           display: 'inline-block',
           backgroundColor: 'red',
-            width: (window.innerWidth / 2) - 20,
-            height: window.innerHeight - 2,
-            cssFloat: 'right',
+          width: (window.innerWidth / 2) - 20,
+          height: window.innerHeight - 2,
+          cssFloat: 'right',
         }}>
-          <MuiChat chatController={chatCtl} />
+        <MuiChat chatController={chatCtl} />
       </div>
     </div>
   );
